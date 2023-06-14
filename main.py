da04e4ad979410149d028d0609e379ce4db592e2
thesemeiev <thesemeiev@icloud.com>
2023-06-14 18:17:08 +0300

    Added commands help, start and config file.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN_API
+
+HELP_COMMANDS = """
+/help - Список комманд
+/start - Начать работу с ботом
+"""
 
 #Токен для подключения к телеграм API.
-TOKEN_API = "1887379253:AAFYEPxXuqmk1cXUEBFKaKod_3pX2oQSuJU" 
-
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(text=message.text)
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.reply(text=HELP_COMMANDS)
+    await message.delete()
 
+@dp.message_handler(commands=['start'])
+async def help_command(message: types.Message):
+    await message.answer(text='Добро пожаловать в мой телеграмм бот!')
+    await message.delete()
 
 if __name__ == '__main__':
     executor.start_polling(dp)
